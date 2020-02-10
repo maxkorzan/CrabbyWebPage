f64a54a6ecc12fbafdb70bcc4f02927d073e7276
maxkorzan <maxkorzan@gmail.com>
2020-02-10 10:25:36 -0500

    first commit

---
+++ src/main/java/com/max/crabbywebpage/CrabbywebpageApplication.java
@@ -0,0 +1,13 @@
+package com.max.crabbywebpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrabbywebpageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrabbywebpageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/max/crabbywebpage/CrabbywebpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.max.crabbywebpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrabbywebpageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
